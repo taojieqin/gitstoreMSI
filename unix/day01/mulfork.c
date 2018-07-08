7e7b8bc2944f737736e0ef6972da4ee1f42689b6
taojieqin <550999308@qq.com>
2018-07-08 11:28:11 +0800

    change day01 mulfork.c

--- unix/day01/mulfork.c
+++ unix/day01/mulfork.c
@@ -1,12 +1,14 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-
-
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <signal.h>
+#include <errno.h>
 
 void TestFun()
 {
-	printf("fff\n");
+	printf("child\n");
 }
 
 
@@ -15,6 +17,7 @@ int main()
 	int procnum =10;
 	int loopnum = 100;
 	int i =0;
+	int ret;
 	pid_t pid;
 
 	for(i=0; i< procnum; i++)
@@ -26,7 +29,21 @@ int main()
 			exit(0);
 		}
 	}
-	printf("hello \n");
+	
+
+// ?????????????????????????????????
+	while(1)
+	{
+		ret = wait(NULL);
+		if(ret == -1)
+		{
+			if(errno == EINTR)
+				continue;
+			break;
+		}
+	}
+	printf("parent quit \n");
 
 	return 0;
 }
+
